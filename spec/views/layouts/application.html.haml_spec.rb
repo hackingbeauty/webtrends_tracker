959db8d4b49229d4d7527287f55f83741ef96076
Mark Muskardin <mmuskardin@srwebanalyst.corp.primedia.com>
2009-10-14 08:49:18 -0400

    [CV/MM] Render logout link in header if logged in - Do not render logout link if user is not logged in

--- spec/views/layouts/application.html.haml_spec.rb
+++ spec/views/layouts/application.html.haml_spec.rb
@@ -2,34 +2,43 @@ require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
 
 describe "layouts/application" do
  
+  before :each do
+    @products = ["AG", "RH", "NHG"].map { |name| mock_model(Product, :name => name) } 
+    assigns[:products] = @products
+    template.stub!(:logged_in?).and_return(true)
+  end
+
   def do_render
     render "layouts/application"
   end
   
-  describe "rendering product links" do
-
-    before :each do
-      @products = ["AG", "RH", "NHG"].map { |name| mock_model(Product, :name => name) } 
-      assigns[:products] = @products
-      template.stub!(:logged_in?).and_return(true)
-    end
-
-    it "should not show product links if user is not logged in" do
-      template.stub!(:logged_in?).and_return(false)
-      do_render
-      @products.each do |product|
-        response.should_not have_tag("a[href=?]", product_path(product))
-      end
-    end
-
-    it "should show product links if user is logged in" do
-      do_render
-      @products.each do |product|
-        response.should have_tag("a[href=?]", product_path(product))
-      end
-    end
-    
+  it "should render a logout link if the user is logged in" do
+    do_render
+    response.should have_tag("a", /logout/i)
   end
+
+  it "should not render a logout link if the user is logged in" do
+    template.stub!(:logged_in?).and_return(false)
+    do_render
+    response.should_not have_tag("a", /logout/i)
+  end
+  
+  it "should not show product links if user is not logged in" do
+    template.stub!(:logged_in?).and_return(false)
+    do_render
+    @products.each do |product|
+      response.should_not have_tag("a[href=?]", product_path(product))
+    end
+  end
+
+  it "should show product links if user is logged in" do
+    do_render
+    @products.each do |product|
+      response.should have_tag("a[href=?]", product_path(product))
+    end
+  end
+  
+  
  
  
 end
