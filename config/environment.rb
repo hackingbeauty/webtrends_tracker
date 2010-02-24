ce4243e368bd96ed235f50a6134b446b0d7cbfb2
watchingant <awatson@primedia.com>
2010-02-24 16:25:03 -0500

    changed paginate requirement  {AW}

--- config/environment.rb
+++ config/environment.rb
@@ -10,7 +10,7 @@ Rails::Initializer.run do |config|
   config.gem 'paperclip', :version => '2.3.1'
   config.gem 'authlogic', :version => '2.1.1'
   config.gem 'haml', :version => '2.2.20'
-  config.gem 'will_paginate', :version => '2.3.12'
+  config.gem 'will_paginate', :version => '2.2.2'
   config.gem 'nokogiri', :version => '1.4.1'
   
   # Settings in config/environments/* take precedence over those specified here.
