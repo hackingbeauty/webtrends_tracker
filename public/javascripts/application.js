4092112b748baf1d16dff0230a5d2d549659916c
Mark Muskardin <mmuskardin@railsteam7.local>
2010-02-26 13:58:34 -0500

    [MM/KP] [2582960] Address QA and usability problems w/ app

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,12 +11,10 @@
   	...................................................................... */
   
   var h = function (stg) {                                        
-    return(                                                                 
-      stg.replace(/&/g,'&amp;').                                         
-          replace(/>/g,'&gt;').                                           
-          replace(/</g,'&lt;').                                           
-          replace(/"/g,'&quot;')                                         
-    );                                                                      
+    return stg.replace(/&/g,'&amp;').                                         
+               replace(/>/g,'&gt;').                                           
+               replace(/</g,'&lt;').                                           
+               replace(/"/g,'&quot;');                                                                      
   };
   
   $.fn.zebra_stripe = function(){
@@ -33,13 +31,34 @@
     });
   };
   
+  $.fn.faint_input = function(){
+    $(this).addClass('faint');
+    $(this)
+      .live('focus', function(){
+        var self = $(this);
+        if ( ! self.data('default') ) {
+          self.data('default', self.val());
+        }
+        if ( self.data('default') == self.val() ){
+          self.val('');            
+          self.addClass('normal');
+        }
+    })
+      .live('blur',function(){
+        var self = $(this);
+        if(self.val() == ''){
+          self.val(self.data('default'));
+          self.removeClass('normal');
+          self.addClass('faint'); 
+        }
+    });
+  };
+  
   /*	=Search Object
   	...................................................................... */
   
-  var Search = function(){};
-  
-  Search.prototype = {
-    init: function(){
+  var Search = function(){
+    this.init = function(){
       var input = $('#search-input');
       if( input.length ){
         input.focus(function(){
@@ -59,33 +78,11 @@
   	...................................................................... */
   
   var KeyValueForm = function(){ // constructor function object 
-    this.faint_form_rows = function(){
-      $('#kvp_form input:text').each(function(){
-        $(this).addClass('faint');
-      });
-    },//end faint_form_rows
-    this.form_row_events = function(){
-      $('#kvp_form input:text')
-        .live('focus', function(){
-          var self = $(this);
-          
-          if ( ! self.data('default') ) {
-            self.data('default', self.val());
-          }
-          if ( self.data('default') == self.val() ){
-            self.val('');            
-            self.addClass('normal');
-          }
-      })
-        .live('blur',function(){
-          var self = $(this);
-          if(self.val() == ''){
-            self.val(self.data('default'));
-            self.removeClass('normal');
-            self.addClass('faint'); 
-          }
-      });
-    },//end form_row_events
+    this.init = function(){
+      this.create_key_val();
+      //this.form_row_events();
+       $('#kvp_form input:text').faint_input(); //utility function to make input value faint font
+    },//end init
     this.create_key_val = function(){
       var kvp_form = document.getElementById("new_key_value_pair");
       if(kvp_form){
@@ -159,70 +156,42 @@
   }//end KeyValueForm
   window.PRIMEDIA.KeyValueForm = KeyValueForm;
   
-  /*	=PageViewTag Object
+  
+  /*	=Tag Object (MultitrackTag and PageView Tag will inherit)
   	...................................................................... */
   
-  var PageViewTag = function() { //constructor function object
-    this.form_row_events = function(){
-      $('#pageviews input:text')
-        .live('focus',function(){
-          var self = $(this);
-          if ( ! self.data('default') )
-            self.data('default', self.val());
-          
-          if ( self.data('default') == self.val() ){
-            self.val('');            
-            self.addClass('normal');
-          }
-      })
-        .live('blur',function(){
-          var self = $(this);
-          if(self.val() == ''){
-            self.val(self.data('default'));
-            self.removeClass('normal');
-            self.addClass('faint'); 
-          }
-      }); 
-    },//end form_row_events
+  var Tag = function(tag_type){ //constructor function object
+    this.init = function(){
+      this.create_tag_form_field();
+      this.delete_tag_form_field();
+      this.save_tag();
+    },
     this.create_tag_form_field = function(){
-      var create_pageview_btn = document.getElementById("create-pageview-tag");
-      if(create_pageview_btn){
-        create_pageview_btn.onclick = function(){
-          if($('#pageviews-add').is(':hidden')) {
-            $('#pageviews-add').show();
+      var create_btn = document.getElementById("create_" + tag_type +"_tag");
+      if(create_btn){
+        create_btn.onclick = function(){           
+          if($('#'+ tag_type +'_add').is(':hidden')) {
+            $('#'+ tag_type +'_add').show();
           }
-          var form_row = '<form class="create_page_view_tag" method="post" action="/page_view_tags">' +
-            '<div style="margin:0;padding:0;display:inline"><input name="authenticity_token" type="hidden" value="'+ rails_authenticity_token +'" />' +
-            '<div class="form-row">' +
-            '<ul>' +
-            '<li><input type="text" size="15" name="page_view_tag[location]" class="page_view_tag_location" value="location" autocomplete="off" /></li>' +
-            '<li><input type="text" size="15" name="page_view_tag[description]" class="page_view_tag_description" value="description" autocomplete="off" /></li>' +
-            '<li><input class="hidden" class="page_view_tag_product_id" name="page_view_tag[product_id]" type="hidden" value="'+ PRIMEDIA_product_id +'" /></li>' +
-            '<li><a class="clear button">Clear</a></li>' +
-            '<li><input type="submit" class="button submit-pageview-btn" value="Save" /></li>' +
-            '</ul>' +
-            '</div>' +
-            '</form>';          
-          $('#pageviews-add').after(form_row);
-          $('#pageviews input:text').each(function(){
-            $(this).addClass('faint');
-          });    
+          var form_row = $('#'+ tag_type +'_form_snippet').children(':first-child').clone().show();         
+          $('#'+ tag_type +'_add').after(form_row); //add the form row after the div '<tag_type>_add'
+          $('#'+ tag_type + ' input:text:visible').faint_input(); //utility function to make input value faint font
           return false;
         }
-      }
-    },//end create_tag
-    this.delete_pageview_tag_form_row = function(){
-      $('.clear').live('click', function(){
-        var form_row = $(this).parents('form.create_page_view_tag');
+      } 
+    },//end create_tag_form_field
+    this.delete_tag_form_field = function(){
+      $('#'+tag_type+' .clear').live('click', function(){
+        var form_row = $(this).parents('form.create_' + tag_type + '_tag');
         $(form_row).fadeOut(300, function(){ $(this).remove(); });
       })
-    },//end delete_pageview_tag_form_row
-    this.save_page_view_tag = function(){
-      $('form.create_page_view_tag').live('submit',function(){
+    },//end delete_tag_form_field
+    this.save_tag = function(){     
+      $('form.create_'+ tag_type +'_tag').live('submit',function(){
         var self = $(this);
         $.ajax({
           type: 'post',
-          url: '/page_view_tags/',
+          url: '/'+tag_type+'_tags/',
           data: self.serialize(),
           dataType: 'json',
           error: function(res) { 
@@ -238,23 +207,45 @@
            setTimeout(function(){ error_container.slideUp(500); }, 8000);
           },
           success: function(res){
-            var page_view_tag = res.page_view_tag;
-
-            var new_row = $('<tr><td><a href="/page_view_tags/' + page_view_tag.id +'">' + page_view_tag.location + '</a></td>' +
-                  '<td>' + page_view_tag.description + '</td>' + 
-                  '<td>PageViewTag</td>' +
-                  '<td><a class="delete button" href="/page_view_tags/'+page_view_tag.id +'">Delete</a></td></tr>d');
+            var tag, tag_type_name, tag_sort_key, tag_sort_key_name;
+            if(tag_type == "page_view"){
+              tag = res.page_view_tag;
+              tag_type_name = "PageViewTag"; // mimic ruby class
+              tag_sort_key = tag.location;
+              tag_sort_key_name = "location";
+            } else {
+              tag = res.multitrack_tag;
+              tag_type_name = "MultitrackTag";
+              tag_sort_key = tag.hook;
+              tag_sort_key_name = "hook";
+            }
+              
+            var row_stg = '<tr>';
             
-            var rows = $('table#page-view-tags-table tbody tr');
+              if( tag_type == 'multitrack' ){ 
+                row_stg += '<td><a href="/' + tag_type + '_tags/' + tag.id +'">' + tag.hook + '</a></td>';
+                row_stg += '<td>' + tag.location + '</td>';
+              } else {
+                row_stg += '<td><a href="/' + tag_type + '_tags/' + tag.id +'">' + tag.location + '</a></td>';
+              }
+              
+              row_stg += '<td>' + tag.description + '</td>' + 
+              '<td>' + tag_type_name + '</td>' +
+              '<td><a class="delete button" href="/' + tag_type + '_tags/'+ tag.id +'">Delete</a></td>' +
+              '</tr>';
+              
+            var new_row = $(row_stg);
+            
+            var rows = $('table#'+tag_type+'_tags_table tbody tr');
             var hook_arr = [];
             
             rows.each(function(index){
               hook_arr.push($('td:first a', this).text()); // create an array of all locations
             }); 
             
-            hook_arr.push(page_view_tag.location);
+            hook_arr.push(tag_sort_key);
             hook_arr.sort();
-            var index = hook_arr.indexOf(page_view_tag.location); // find where in the sorted array the newly added hook exists
+            var index = hook_arr.indexOf(tag_sort_key); // find where in the sorted array the newly added hook/location exists
             var prev_row = $(rows[index - 1]);
             
             if( rows.length && prev_row.length == 0 ){
@@ -262,12 +253,12 @@
             } else if ( rows.length ) {
               new_row.insertAfter(prev_row);
             } else {
-              new_row.appendTo($('#page-view-tags-table tbody'));
+              new_row.appendTo($('#'+tag_type+'tags_table tbody'));
             }
             
-            $('#page-view-tags-table').zebra_stripe();
+            $('#'+tag_type+'_tags_table').zebra_stripe();
             
-            $('a[href=/page_view_tags/' + page_view_tag.id + ']:last').click(function(){
+            $('a[href=/'+tag_type+'_tags/' + tag.id + ']:last').click(function(){
                var sure = confirm("Are you sure?");
                if ( ! sure ) {
                  return false;
@@ -277,7 +268,10 @@
                    data: { '_method': 'delete', 'authenticity_token' : rails_authenticity_token },
                    url: this.href,
                    success: function(){
-                     new_row.fadeOut(1000, function(){ $(this).remove(); });
+                     new_row.fadeOut(1000, function(){ 
+                       new_row.remove(); 
+                       $('#'+tag_type+'_tags_table').zebra_stripe(); 
+                     });
                    }
                  });//end ajax
                }//end if
@@ -286,180 +280,29 @@
             
             self.fadeOut(400, function(){
               $(this).remove(); // remove from inputs from dom
-              $('.page_view_tag_location:first').focus(); // focus the first pageview input
+              $('.'+tag_type+'_tag_' + tag_sort_key_name + ':first').focus(); // focus the first pageview input
             });
           }//end success
         });//end ajax
         return false;
       })//end live
-    }// end save_page_view_tag
+    }//end save_tag
   }
-  window.PRIMEDIA.PageViewTag = PageViewTag;
+  window.PRIMEDIA.Tag = Tag;
   
-  
-  /*	=MultitrackTag Object
-  	...................................................................... */
-  	
-  var MultitrackTag = function(){ // constructor function object
-    this.form_row_events = function(){
-      $('#multitracks input:text')
-        .live('focus',function(){
-          var self = $(this);
-          if ( ! self.data('default') )
-            self.data('default', self.val());
-
-          if ( self.data('default') == self.val() ){
-            self.val('');            
-            self.addClass('normal');
-          }
-      })
-        .live('blur',function(){
-          var self = $(this);
-          if(self.val() == ''){
-            self.val(self.data('default'));
-            self.removeClass('normal');
-            self.addClass('faint'); 
-          }
-      }); 
-    },//end form_row_events
-    this.create_tag_form_field = function(){
-      var create_multitrack_btn = document.getElementById("create-multitrack-tag");
-      if(create_multitrack_btn){        
-        create_multitrack_btn.onclick = function(){
-          if($('#multitracks-add').is(':hidden')) {
-            $('#multitracks-add').show();
-          }
-          $('#multitracks-add').after('<form class="create_multitrack_tag" method="post" action="/multitrack_tags">' +
-            '<div style="margin:0;padding:0;display:inline"><input name="authenticity_token" type="hidden" value="'+ rails_authenticity_token +'" />' +
-            '<div class="form-row">' +
-            '<ul>' +
-            '<li><input type="text" size="10" name="multitrack_tag[hook]" class="multitrack_tag_hook" value="hook" autocomplete="off" /></li>' +
-            '<li><input type="text" size="15" name="multitrack_tag[location]" class="multitrack_tag_location" value="location" autocomplete="off" /></li>' +
-            '<li><input type="text" size="15" name="multitrack_tag[description]" class="multitrack_tag_description" value="description" autocomplete="off"/></li>' +
-            '<li><input class="hidden" class="multitrack_tag_product_id" name="multitrack_tag[product_id]" type="hidden" value="'+ PRIMEDIA_product_id +'" /></li>' +
-            '<li><a class="clear button">Clear</a></li>' +
-            '<li><input type="submit" class="button submit-multitrack-btn" value="Save" /></li>' +
-            '</ul>' +
-            '</div>' +
-            '</form>');
-            $('form.create_multitrack_tag input:text').each(function(){//make input value of inputs faint
-              $(this).addClass("faint");
-            });
-          return false;
-        }
-      }
-    },//end create_tag
-    this.delete_multitrack_tag_form_row = function(){
-      $('.clear').live('click', function(){
-        var form_row = $(this).parents('form.create_multitrack_tag');
-        $(form_row).fadeOut(300, function(){ $(this).remove(); });
-      })
-    },//end delete_multitrack_tag_form_row
-    
-    this.save_multitrack_tag = function(){
-      $('form.create_multitrack_tag').live('submit',function(){
-        var self = $(this);
-        $.ajax({
-          type: 'post',
-          url: '/multitrack_tags/',
-          data: self.serialize(),
-          dataType: 'json',
-          error: function(res) { 
-            var errors = JSON.parse(res.responseText);
-            var error_list = $('<ul>');
-            var error_container = $('#error_container');
-            error_container.html('');
-            
-            for(var i=0, j=errors.length; i < j; i++)
-              $('<li>' + errors[i] + '</li>').appendTo(error_list);
-            
-            error_list.appendTo(error_container);
-            error_container.slideDown(400);
-            setTimeout(function(){ error_container.slideUp(500); }, 8000);
-          },
-          success: function(res){
-            var multitrack_tag = res.multitrack_tag;
-                          
-            var new_row = $('<tr><td><a href="/multitrack_tags/' + multitrack_tag.id + '">' + multitrack_tag.hook + '</a></td>' +
-                  '<td>' + multitrack_tag.location + '</td>' + 
-                  '<td>' + multitrack_tag.description + '</td>' + 
-                  '<td>MultitrackTag</td>' +
-                  '<td><a class="delete button" href="/multitrack_tags/'+ multitrack_tag.id +'">Delete</a></td></tr>d')      
-            
-            var rows = $('table#multitrack-tags-table tbody tr');
-            var hook_arr = [];
-            
-            rows.each(function(index){
-              hook_arr.push($('td:first a', this).text()); // create an array of all multitrack hooks
-            }); 
-            
-            hook_arr.push(multitrack_tag.hook);
-            hook_arr.sort();
-            var index = hook_arr.indexOf(multitrack_tag.hook); // find where in the sorted array the newly added hook exists
-            var prev_row = $(rows[index - 1]);
-                        
-            if( rows.length && prev_row.length == 0 ){
-              new_row.insertBefore($(rows[0]));
-            } else if ( rows.length ) {
-              new_row.insertAfter(prev_row);
-            } else {
-              new_row.appendTo($('#multitrack-tags-table tbody'));
-            }
-            
-            $('#multitrack-tags-table').zebra_stripe();            
-            
-            //AJAX delete
-            $('a[href=/multitrack_tags/' + multitrack_tag.id + ']:last').click(function(){
-               var sure = confirm("Are you sure?");
-               if ( ! sure ) {
-                 return false;
-               } else {
-                 $.ajax({
-                   type: 'post',
-                   data: { '_method': 'delete', 'authenticity_token' : rails_authenticity_token },
-                   url: this.href,
-                   success: function(){
-                     new_row.fadeOut(1000, function(){ $(this).remove(); });
-                   }
-                 });//end ajax
-               }//end if
-               return false;
-             });//end click
-            
-            self.fadeOut(400, function(){
-              $(this).remove(); // remove from inputs from dom
-              $('.multitrack_tag_hook:first').focus(); // focus the first multitrack input
-            });
-          }//end success
-        });//end ajax
-        return false;
-      })//end live
-    }//end save_multitrack_tag
-    
-  }//end MultitrackTag object
-  window.PRIMEDIA.MultitrackTag = MultitrackTag;
-
 })();
 
 $(document).ready(function() {
   
   var kvp = new PRIMEDIA.KeyValueForm();
-  kvp.create_key_val();
-  kvp.faint_form_rows();
-  kvp.form_row_events();
+  kvp.init();
   
-  var pt = new PRIMEDIA.PageViewTag();
-  pt.create_tag_form_field();
-  pt.delete_pageview_tag_form_row();
-  pt.save_page_view_tag();
-  pt.form_row_events();
+  var mt = new PRIMEDIA.Tag("multitrack");
+  mt.init();
   
-  var mt = new PRIMEDIA.MultitrackTag();
-  mt.create_tag_form_field();
-  mt.delete_multitrack_tag_form_row();
-  mt.save_multitrack_tag();
-  mt.form_row_events();
-  
+  var pt = new PRIMEDIA.Tag("page_view");
+  pt.init();
+
   var s = new PRIMEDIA.Search();
   s.init();
   
